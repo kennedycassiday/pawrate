56accde06b695504af281d286fb41720e8d63a49
Kennedy <kennedycassiday@gmail.com>
2025-08-26 19:35:34 -0700

    added save and discard buttons to end session page

--- pawrate-app/app/end-session.jsx
+++ pawrate-app/app/end-session.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
-import { useLocalSearchParams } from 'expo-router';
+import {TouchableOpacity, StyleSheet, Text, View, Pressable} from 'react-native';
+import { useLocalSearchParams, useRouter} from 'expo-router';
 import Octicons from '@expo/vector-icons/Octicons';
 
 export default function EndSession() {
   const { count } = useLocalSearchParams();
+  const router = useRouter();
+  const saveSession = () => {
+    console.log("session saved!")
+  }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.iconContainer}>
+      <View style={styles.iconContainer}>
         <Octicons name="issue-closed" size={200} color="#FED2E2" />
-      </TouchableOpacity>
+      </View>
       <Text style={styles.text}>{(count * 4) || 0} breaths per minute</Text>
+
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button} onPress={saveSession}>
+          <Text style={styles.buttonText}>Save Session</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => router.push('/begin-session')}>
+          <Text style={styles.buttonText}>Start Over</Text>
+        </Pressable>
+    </View>
     </View>
   );
 }
@@ -33,5 +46,36 @@ const styles = StyleSheet.create({
       },
       iconContainer: {
         padding: 8,
+      },
+      buttonContainer: {
+        width: "90%",
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: 20,
+      },
+      button: {
+        paddingVertical: 15,
+        paddingHorizontal: 25,
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        shadowColor: "#000",
+        marginTop: 20,
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        backgroundColor: "#96CEB4",
+        flex: 1,
+        minHeight: 50,
+      },
+      buttonText: {
+        color: "#FED2E2",
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
       },
     });
