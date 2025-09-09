a815f2b42ca86c725f03a3c642cdadbbbed76efd
Kennedy <kennedycassiday@gmail.com>
2025-09-09 16:33:28 -0700

    added loading state to calculate average view

--- pawrate-app/app/calculate-average.jsx
+++ pawrate-app/app/calculate-average.jsx
@@ -4,90 +4,125 @@ import {
   StyleSheet,
   TouchableOpacity,
   TextInput,
-  Alert,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { router } from "expo-router";
 
 export default function CalculateAverage() {
+  const [sessions, setSessions] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
 
-  const getAverage = async () => {
-    if (!startDate.trim() || !endDate.trim()) {
-      Alert.alert(
-        "Missing Info",
-        "Please choose both a start date and an end date."
-      ); //alert will not work on web
-      return;
-    }
-
-    try {
-      const dogId = await AsyncStorage.getItem("dogId");
-      const response = await fetch(
-        `http://192.168.0.150:8000/sessions/dog/${dogId}`,
-        {
-          method: "GET",
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const dogID = await AsyncStorage.getItem("dogId");
+        const response = await fetch(
+          `http://192.168.0.150:8000/sessions/dog/${dogId}`,
+          {
+            method: "GET",
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Failed to retrieve data from backend");
         }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to retrieve data from backend");
+        const savedSessions = await response.json();
+        console.log(savedSessions);
+        setSessions(savedSessions);
+      } catch (error) {
+        console.error("Error fetching sessions", error);
+      } finally {
+        setLoading(false);
       }
+    };
+  }, []);
 
-      const sessions = await response.json();
-      console.log(sessions);
+  //   const [loading, setLoading] = useState(true);
+  //   const [startDate, setStartDate] = useState("");
+  //   const [endDate, setEndDate] = useState("");
 
-      //   const savedDog = await response.json();
-      //   console.log(savedDog)
-      //   await AsyncStorage.setItem('dogId', savedDog.id.toString());
-    } catch (error) {
-      console.error("Error calculating average:", error);
-      Alert.alert("Error", "Could not calculate average. Please try again.");
-    }
-  };
+  //   const getAverage = async () => {
+  //     if (!startDate.trim() || !endDate.trim()) {
+  //       Alert.alert(
+  //         "Missing Info",
+  //         "Please choose both a start date and an end date."
+  //       ); //alert will not work on web
+  //       return;
+  //     }
+
+  //     try {
+  //       const dogId = await AsyncStorage.getItem("dogId");
+  //       const response = await fetch(
+  //         `http://192.168.0.150:8000/sessions/dog/${dogId}`,
+  //         {
+  //           method: "GET",
+  //         }
+  //       );
+
+  //       if (!response.ok) {
+  //         throw new Error("Failed to retrieve data from backend");
+  //       }
+
+  //       const sessions = await response.json();
+  //       console.log(sessions);
+
+  //         const savedDog = await response.json();
+  //         console.log(savedDog)
+  //         await AsyncStorage.setItem('dogId', savedDog.id.toString());
+  //     } catch (error) {
+  //       console.error("Error calculating average:", error);
+  //       Alert.alert("Error", "Could not calculate average. Please try again.");
+  //     }
+  //   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Please input the desired start and end dates for the time period you
-        wish to average.
-      </Text>
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <Text style={styles.loadingText}>Loading sessions...</Text>
+        </View>
+      ) : (
+        <View>
+          <Text style={styles.text}>
+            Please input the desired start and end dates for the time period you
+            wish to average.
+          </Text>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Start:</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Enter a start date"
-          placeholderTextColor="#96CEB4"
-          value={startDate}
-          onChangeText={setStartDate}
-        />
-      </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Start:</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter a start date"
+              placeholderTextColor="#96CEB4"
+              //   value={startDate}
+              //   onChangeText={setStartDate}
+            />
+          </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>End:</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Enter an end date"
-          placeholderTextColor="#96CEB4"
-          value={endDate}
-          onChangeText={setEndDate}
-        />
-      </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>End:</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter an end date"
+              placeholderTextColor="#96CEB4"
+              //   value={endDate}
+              //   onChangeText={setEndDate}
+            />
+          </View>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={getAverage}
-          style={styles.button}
-          accessibilityRole="link"
-          importantForAccessibility="yes"
-        >
-          <Text style={styles.buttonText}>Get average!</Text>
-        </TouchableOpacity>
-      </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              //   onPress={getAverage}
+              style={styles.button}
+              accessibilityRole="link"
+              importantForAccessibility="yes"
+            >
+              <Text style={styles.buttonText}>Get average!</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -156,5 +191,15 @@ const styles = StyleSheet.create({
     color: "#FED2E2",
     fontSize: 18,
     fontWeight: "bold",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingText: {
+    color: "#FED2E2",
+    fontSize: 18,
+    fontWeight: "600",
   },
 });
